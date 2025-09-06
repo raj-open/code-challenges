007da31c00117f624128af87f21620225eb4e838
raj-open <raj-open@users.noreply.github.com>
2025-09-06 21:27:44 +0200

    story-gs > staging: compute statistics and display only 1st solution

--- src/bin/games/genius_square/features/setup_game.rs
+++ src/bin/games/genius_square/features/setup_game.rs
@@ -3,6 +3,8 @@
 /// ----------------------------------------------------------------
 
 use rand_chacha::ChaCha8Rng;
+use std::time::Duration;
+use std::time::SystemTime;
 
 use crate::models::dice::methods::roll_dice;
 use crate::models::dice::models::Die;
@@ -15,6 +17,8 @@ use crate::algorithms::solve::solve_brute_force;
 /// METHODS
 /// ----------------------------------------------------------------
 
+const TIMEOUT: Duration = Duration::from_secs(10);
+
 pub fn feature_setup_game(
     rng: &mut ChaCha8Rng,
     option_roll: Option<Vec<String>>,
@@ -24,7 +28,7 @@ pub fn feature_setup_game(
     let dice: Vec<Die> = faces.iter()
         .map(|face| Die::from_string(face))
         .collect();
-    println!("\nRoll: {}.\n", faces.join(", "));
+    println!("\nRoll: {}.", faces.join(", "));
 
     // Establish the problem
     let coords = dice.iter().map(|die| die.to_coords()).collect();
@@ -33,25 +37,34 @@ pub fn feature_setup_game(
     println!("\nProblem:\n{}", board.pretty());
 
     // Solve the problem
-    println!("\nCompute solution ...\n");
-    let rx = solve_brute_force(&board);
+    print!("\nCompute solution ... ");
+    let rx = solve_brute_force(&board, true);
+    let mut solution: Option<GameBoard> = None;
+    let mut dt: Option<Duration> = None;
     let mut n = 0;
-    #[allow(while_true)]
-    while true {
-        match rx.recv() {
-            Ok((dt, board)) => {
-                n += 1;
-                println!("... completed in {:.2?}", dt);
-                println!("\nSolution {n}:\n{}\n", board.pretty());
-                // NOTE: Currently stop on first solution
-                // break;
-            },
-            Err(_) => {
-                break;
-            }
+    let time = SystemTime::now();
+
+    while let Ok(board) = rx.recv_timeout(TIMEOUT) {
+        if n == 0 {
+            dt = Some(time.elapsed().unwrap());
+            solution = Some(board);
         }
+        n += 1;
     }
-    if n == 0 {
-        println!("\n\x1b[91mNo solution found!\x1b[0m\n");
+
+    let dt_total = time.elapsed().unwrap();
+    let dt_mean: Duration = if n > 0 {dt_total/n} else {Duration::from_secs(0)};
+    let dt = dt.unwrap_or(dt_total);
+    match solution {
+        Some(board) => {
+            println!("found {n} solutions.");
+            println!("Time for 1st solution:      {dt:2?}");
+            println!("Average time per solution:  {dt_mean:2?}");
+            println!("Total time:                 {dt_total:2?}");
+            println!("\nSolution 1:\n{}\n", board.pretty());
+        },
+        None => {
+            println!("\x1b[91mno solution found!\x1b[0m\n");
+        }
     }
 }
