1e11030232ba69adbd22614b5f745097164a4f27
raj-open <raj-open@users.noreply.github.com>
2025-09-07 19:05:05 +0200

    story-gs > staging: multiline join with multiline sep

--- src/_core/strings.rs
+++ src/_core/strings.rs
@@ -35,50 +35,70 @@ pub fn purify_string_length(text: &String) -> usize {
 /// Performs a horizontal string join for multline strings
 pub fn join_multiline_strings(
     blocks: &Vec<String>,
-    sep: &str,
+    sep_block: Option<&String>,
+    sep_single: &str,
 ) -> String {
+
     // split blocks into lines and pad to ensure consistency of widths
-    let re = Regex::new(r"\r?\n").unwrap();
-    let blocks: Vec<Vec<String>> = blocks
-        .iter()
-        .map(|x| {
-            let lines: Vec<String> = re.split(x).map(|x| x.to_string()).collect();
-            let width = lines.iter().map(purify_string_length).max().unwrap_or(0);
-            let lines: Vec<String> = lines
-                .iter()
-                .map(|x| {
-                    let n = purify_string_length(x);
-                    let space = " ".repeat(width - n);
-                    return format!("{x}{space}");
-                })
-                .collect();
-            return lines;
-        })
-        .collect();
+    let blocks: Vec<Vec<String>> = blocks.iter().map(pad_widths).collect();
 
     // pad to ensure consistencey of heights
     let height = blocks.iter().map(|lines| lines.len()).max().unwrap_or(0);
     let blocks: Vec<Vec<String>> = blocks
         .iter()
-        .map(|lines| {
-            let n = lines.len();
-            let line = lines.get(0).map(|x| x.clone()).unwrap_or("".to_string());
-            let width = purify_string_length(&line);
-            let space = " ".repeat(width);
-            let pad = vec![space.clone(); height - n];
-            let lines = [lines.to_owned(), pad].concat();
-            return lines;
-        })
+        .map(|lines| pad_height(lines, height))
         .collect();
 
+    // determine a separator
+    let sep_block0 = ([sep_single].repeat(height)).join("\n");
+    let sep_block = sep_block.map_or_else(|| sep_block0, |x| x.clone());
+    let sep_block = pad_widths(&sep_block);
+    let sep_block = pad_height(&sep_block, height);
+
+    // join blocks line-wise
     let result = (0.. height)
         .map(|i| {
+            let empty = "".to_string();
+            let sep = sep_block.get(i).unwrap_or(&empty);
             blocks
             .iter()
-            .map(|lines| lines.get(i).unwrap())
+            .map(|lines| lines.get(i).unwrap_or(&empty))
             .join(sep)
         })
         .join("\n");
 
     return result;
+}
+
+/// Splits a string into lines and pads to ensure uniformity of widths
+fn pad_widths(text: &String) -> Vec<String> {
+    let re = Regex::new(r"\r?\n").unwrap();
+    let lines: Vec<String> = re.split(text).map(|x| x.to_string()).collect();
+    let width = lines.iter().map(purify_string_length).max().unwrap_or(0);
+    let lines: Vec<String> = lines
+        .iter()
+        .map(|x| {
+            let n = purify_string_length(x);
+            let space = " ".repeat(width - n);
+            return format!("{x}{space}");
+        })
+        .collect();
+    return lines;
+}
+
+/// Splits a string into lines and pads to ensure uniformity of widths
+fn pad_height(
+    lines: &Vec<String>,
+    height: usize,
+) -> Vec<String> {
+    let n = lines.len();
+    if n >= height {
+        return lines.clone();
+    }
+    let line = lines.get(0).map(|x| x.clone()).unwrap_or("".to_string());
+    let width = purify_string_length(&line);
+    let space = " ".repeat(width);
+    let pad = vec![space.clone(); height - n];
+    let lines = [lines.to_owned(), pad].concat();
+    return lines;
 }
